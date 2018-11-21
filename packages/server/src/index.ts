8c6a88e9afd26dcda4cae525f4c3b907e358c831
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-11-21 18:37:26 -0500

    origin push

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,6 +3,7 @@ import { ApolloServer } from "apollo-server-express";
 import * as express from "express";
 import * as session from "express-session";
 import * as connectRedis from "connect-redis";
+import { redis } from "./redis";
 
 import { createTypeormConnection } from "./createTypeormConnection";
 import { createSchema } from "./createSchema";
@@ -16,10 +17,28 @@ const startServer = async () => {
 
   const app = express();
 
-  app.use();
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis as any
+      }),
+      name: "qid",
+      secret: SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 7 // years
+      }
+    })
+  );
 
   const server = new ApolloServer({
-    schema: createSchema()
+    schema: createSchema(),
+    context: ({ req }) => ({
+      req
+    })
   });
 
   server.applyMiddleware({ app });
