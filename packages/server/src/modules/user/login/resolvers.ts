e87be65ef1f62fa3fe0fdb0c6e341448ce7a1414
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-11-21 17:35:31 -0500

    created login resolver error response

---
+++ packages/server/src/modules/user/login/resolvers.ts
@@ -0,0 +1,58 @@
+import * as argon from "argon2";
+
+// import { MutationResolvers } from "../../../types";
+import { User } from "../../../entity/User";
+import { getConnection } from "typeorm";
+// import { registerSchema } from "@code-review/common";
+// import { formatYupErrors } from "../../../utils/formatYupErrors";
+
+const invalidLoginResponse = {
+  errors: [
+    {
+      path: "password",
+      message: "invalid login"
+    }
+  ],
+  user: null
+};
+
+export const resolvers = {
+  login: async (_, { input }) => {
+    const user = await getConnection()
+      .getRepository(User)
+      .createQueryBuilder("user")
+      .where("user.email = :email", {
+        email: input.usernameOrEmail
+      })
+      .orWhere("user.username = :username", {
+        username: input.usernameOrEmail
+      })
+      .getOne();
+
+    if (!user) {
+      return invalidLoginResponse;
+    }
+
+    const valid = argon.verify(user.password, input.password);
+
+    if (!valid) {
+      return invalidLoginResponse;
+    }
+
+    if (!valid) {
+    }
+
+    console.log(user);
+
+    return {
+      errors: [],
+      user: null
+    };
+  }
+};
+
+export default {
+  Mutation: {
+    ...resolvers
+  }
+};
